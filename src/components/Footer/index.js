729070c28747ffc43a35e74244a4f1fb281013d6
Lopes <57339312+Andre1999Lopes@users.noreply.github.com>
2020-09-14 22:06:07 -0300

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,9 +8,10 @@ function Footer() {
         <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
       </a>
       <p>
-        Orgulhosamente criado por <a target="_blank" href="https://www.linkedin.com/in/andré-júnior-lopes-cardoso-0a3838197/">André</a> durante a
+        Orgulhosamente criado por 
+        <a target="_blank" href="https://www.linkedin.com/in/andré-júnior-lopes-cardoso-0a3838197/">André</a> durante a
         {' '}
-        <a href="https://www.alura.com.br/">
+        <a target="_blank" href="https://www.alura.com.br/">
           Imersão React da Alura
         </a>
       </p>
