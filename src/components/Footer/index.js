0ef0a2bdf4ce6d8ca1cd9d78b42dfaa4be5fd5ce
Lopes <57339312+Andre1999Lopes@users.noreply.github.com>
2020-09-14 22:01:47 -0300

    Update index.js

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -8,7 +8,7 @@ function Footer() {
         <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
       </a>
       <p>
-        Orgulhosamente criado durante a
+        Orgulhosamente criado por <a href="https://www.linkedin.com/in/andré-júnior-lopes-cardoso-0a3838197/">André</a> durante a
         {' '}
         <a href="https://www.alura.com.br/">
           Imersão React da Alura
